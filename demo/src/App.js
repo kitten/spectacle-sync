a2bc758dc019610c9e13c8d8380f6376c11e145f
Phil Plückthun <phil@plckthn.me>
2017-12-06 04:02:13 +0000

    Add demo and updated GIF

---
+++ demo/src/App.js
@@ -0,0 +1,262 @@
+import React from 'react';
+
+import {
+  Appear, BlockQuote, Cite, CodePane, ComponentPlayground, Deck, Fill,
+  Heading, Image, Layout, Link, ListItem, List, Markdown, MarkdownSlides, Quote, Slide, SlideSet,
+  TableBody, TableHeader, TableHeaderItem, TableItem, TableRow, Table, Text, S
+} from 'spectacle';
+
+import NetworkSync from 'spectacle-sync';
+
+import preloader from 'spectacle/lib/utils/preloader';
+import createTheme from 'spectacle/lib/themes/default';
+import Interactive from './spectacle-example-files/interactive';
+
+const images = {
+  city: '/spectacle-example-files/city.jpg',
+  kat: '/spectacle-example-files/kat.png',
+  logo: '/spectacle-example-files/formidable-logo.svg',
+  markdown: '/spectacle-example-files/markdown.png'
+};
+
+preloader(images);
+
+const theme = createTheme({
+  primary: '#ff4081'
+});
+
+export default class Presentation extends React.Component {
+  state = {
+    steps: 0
+  }
+
+  updateSteps = steps => {
+    if (this.state.steps !== steps) { // eslint-disable-line no-invalid-this
+      this.setState({ steps }); // eslint-disable-line no-invalid-this
+    }
+  }
+
+  render() {
+    return (
+      <NetworkSync>
+        <Deck transition={['zoom', 'slide']} theme={theme} transitionDuration={500}>
+          <Slide transition={['zoom']} bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1} textColor="black">
+              Spectacle
+            </Heading>
+            <Heading size={1} fit caps>
+              A ReactJS Presentation Library
+            </Heading>
+            <Heading size={1} fit caps textColor="black">
+              Where You Can Write Your Decks In JSX
+            </Heading>
+            <Link href="https://github.com/FormidableLabs/spectacle">
+              <Text bold caps textColor="tertiary">View on Github</Text>
+            </Link>
+            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
+          </Slide>
+          <Slide
+            id="wait-what"
+            transition={[
+              'fade',
+              (transitioning, forward) => {
+                const angle = forward ? -180 : 180;
+                return {
+                  transform: `
+                    translate3d(0%, ${transitioning ? 100 : 0}%, 0)
+                    rotate(${transitioning ? angle : 0}deg)
+                  `,
+                  backgroundColor: transitioning ? '#26afff' : '#000'
+                };
+              }
+            ]}
+            bgColor="black"
+            notes="You can even put notes on your slide. How awesome is that?"
+          >
+            <Image src={images.kat} margin="0px auto 40px" />
+            <Heading size={2} caps fit textColor="primary" textFont="primary">
+              Wait what?
+            </Heading>
+          </Slide>
+          <Slide
+            transitionIn={['zoom', 'fade']}
+            transitionOut={['slide', 'fade']}
+            bgColor="primary"
+            notes="<ul><li>talk about that</li><li>and that</li></ul>"
+          >
+            <CodePane
+              lang="jsx"
+              source={require('./spectacle-example-files/deck.example')}
+              margin="20px auto"
+              overflow = "overflow"
+            />
+          </Slide>
+          <Slide>
+            <ComponentPlayground
+              theme="dark"
+            />
+          </Slide>
+          <Slide transition={['slide']} bgImage={images.city} bgDarken={0.75}>
+            <Appear fid="1">
+              <Heading size={1} caps fit textColor="primary">
+                Full Width
+              </Heading>
+            </Appear>
+            <Appear fid="2">
+              <Heading size={1} caps fit textColor="tertiary">
+                Adjustable Darkness
+              </Heading>
+            </Appear>
+            <Appear fid="3">
+              <Heading size={1} caps fit textColor="primary">
+                Background Imagery
+              </Heading>
+            </Appear>
+          </Slide>
+          <Slide transition={['slide']} bgDarken={0.75} getAppearStep={this.updateSteps}>
+            <Appear>
+              <Heading size={1} caps textColor="tertiary">
+                Can
+              </Heading>
+            </Appear>
+            <Appear>
+              <Heading size={1} caps textColor="secondary">
+                Count
+              </Heading>
+            </Appear>
+            <Appear>
+              <Heading size={1} caps textColor="tertiary">
+                Steps
+              </Heading>
+            </Appear>
+              <Heading size={1} caps fit textColor="secondary">
+                Steps: {this.state.steps}
+              </Heading>
+          </Slide>
+          <Slide transition={['zoom', 'fade']} bgColor="primary">
+            <Heading caps fit>Flexible Layouts</Heading>
+            <Layout>
+              <Fill>
+                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
+                  Left
+                </Heading>
+              </Fill>
+              <Fill>
+                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
+                  Right
+                </Heading>
+              </Fill>
+            </Layout>
+          </Slide>
+          <Slide transition={['slide']} bgColor="black">
+            <BlockQuote>
+              <Quote>Wonderfully formatted quotes</Quote>
+              <Cite>Ken Wheeler</Cite>
+            </BlockQuote>
+          </Slide>
+          <Slide transition={['spin', 'zoom']} bgColor="tertiary">
+            <Heading caps fit size={1} textColor="primary">
+              Inline Markdown
+            </Heading>
+            <Markdown>
+              {`
+    ![Markdown Logo](${images.markdown})
+
+    You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
+    * Lists too!
+    * With ~~strikethrough~~ and _italic_
+    * And let's not forget **bold**
+              `}
+            </Markdown>
+          </Slide>
+          {
+            MarkdownSlides`
+  #### Create Multiple Slides in Markdown
+  All the same tags and elements supported in <Markdown /> are supported in MarkdownSlides.
+  ---
+  Slides are separated with **three dashes** and can be used _anywhere_ in the deck. The markdown can either be:
+  * A Tagged Template Literal
+  * Imported Markdown from another file
+            `
+          }
+          <Slide transition={['slide', 'spin']} bgColor="primary">
+            <Heading caps fit size={1} textColor="tertiary">
+              Smooth
+            </Heading>
+            <Heading caps fit size={1} textColor="secondary">
+              Combinable Transitions
+            </Heading>
+          </Slide>
+          <SlideSet>
+            <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+              <List>
+                <Appear><ListItem>Inline style based theme system</ListItem></Appear>
+                <Appear><ListItem>Autofit text</ListItem></Appear>
+                <Appear><ListItem>Flexbox layout system</ListItem></Appear>
+                <Appear><ListItem>PDF export</ListItem></Appear>
+                <Appear><ListItem>And...</ListItem></Appear>
+              </List>
+            </Slide>
+            <Slide transition={['slide']} bgColor="primary">
+              <Heading size={1} caps fit textColor="tertiary">
+                Your presentations are interactive
+              </Heading>
+              <Interactive/>
+            </Slide>
+          </SlideSet>
+          <Slide transition={['slide']} bgColor="primary"
+            notes="Hard to find cities without any pizza"
+          >
+            <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
+              Pizza Toppings
+            </Heading>
+            <Layout>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHeaderItem/>
+                    <TableHeaderItem>2011</TableHeaderItem>
+                    <TableHeaderItem>2013</TableHeaderItem>
+                    <TableHeaderItem>2015</TableHeaderItem>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableItem>None</TableItem>
+                    <TableItem>61.8%</TableItem>
+                    <TableItem>39.6%</TableItem>
+                    <TableItem>35.0%</TableItem>
+                  </TableRow>
+                  <TableRow>
+                    <TableItem>Pineapple</TableItem>
+                    <TableItem>28.3%</TableItem>
+                    <TableItem>54.5%</TableItem>
+                    <TableItem>61.5%</TableItem>
+                  </TableRow>
+                  <TableRow>
+                    <TableItem>Pepperoni</TableItem>
+                    <TableItem/>
+                    <TableItem>50.2%</TableItem>
+                    <TableItem>77.2%</TableItem>
+                  </TableRow>
+                  <TableRow>
+                    <TableItem>Olives</TableItem>
+                    <TableItem/>
+                    <TableItem>24.9%</TableItem>
+                    <TableItem>55.9%</TableItem>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Layout>
+          </Slide>
+          <Slide transition={['spin', 'slide']} bgColor="tertiary">
+            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
+              Made with love in Seattle by
+            </Heading>
+            <Link href="http://www.formidable.com"><Image width="100%" src={images.logo}/></Link>
+          </Slide>
+        </Deck>
+      </NetworkSync>
+    );
+  }
+}
